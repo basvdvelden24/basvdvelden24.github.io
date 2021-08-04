3522acc7202975eaf09d1c653a16054226461093
JoravDu <53528905+JoravDu@users.noreply.github.com>
2021-08-04 17:44:35 +0200

    update

--- js/rms.js
+++ js/rms.js
@@ -1,16 +1,14 @@
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }), dark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
     maxZoom: 20,
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
 
 var map = L.map(
     "rms", { //<-- nieuw id geven
     center: [52.1292125,5.4497806], //coordinaten aanpassen
     zoom: 7,
-    layers: [osm]
+    layers: [osm],
 });
 
 var baseMaps = {
@@ -18,21 +16,16 @@ var baseMaps = {
     "Dark mode": dark
 }
 
-// alles namen veranderen gangesDelta deltaGanges
-// var rmsDelta = L.geoJson(deltarms, {
-//     style: function (feature) {
-//         return {
-//             color: '#43429c',
-//             weight: 1,
-//             fillOpacity: 1,
-//         }
-//     }
-// }).addTo(map);
+var wmsLayer = L.tileLayer.wms('https://geodata.nationaalgeoregister.nl/rws/kaderrichtlijnwateractueel/wms/v1_0?service=WMS&request=GetCapabilities', {
+    layers: 'krw_oppervlaktewaterlichamen_rws_act_v',
+    opacity: 0.5,
+});
 
-// var overLay = {
-//     "Data": gangesDelta
-// }
+
+var overLay = {
+    "Data": wmsLayer
+}
 
 map.scrollWheelZoom.disable();
 
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overLay).addTo(map);
