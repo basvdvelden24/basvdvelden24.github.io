4c0a724167db0b62b2d40db97727962295ddd3e4
JoravDu <53528905+JoravDu@users.noreply.github.com>
2021-08-09 15:53:38 +0200

    update

--- js/chartjs/chartganges.js
+++ js/chartjs/chartganges.js
@@ -1,4 +1,4 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('chartganges').getContext('2d');
 
 var chart = new Chart(ctx, {
         // The type of chart we want to create
@@ -9,27 +9,27 @@ var chart = new Chart(ctx, {
             labels: ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'],
             datasets: [{
                 type: "bar",
-                label: "Gemiddelde dagen neerslag per maand Londen",
+                label: "Gemiddelde hoeveelheid neerslag (mm) per maand Bangladesh",
                 backgroundColor: 'rgb(217,79,151,0.3)',
                 borderColor: 'rgb(217,79,151)',
                 borderWidth: '2',
                 pointRadius: '0',
                 fill: true,
                 yAxisID: 'Y-axis-1',
-                data: [100, 8.5, 9.3, 9.1, 8.8, 8.2, 7.7, 7.5, 8.1, 10.8, 10.7, 10.2]
+                data: [5, 10, 30, 55, 235, 600, 680, 470, 350, 200, 35, 15]
             }, {
                 type: "bar",
-                label: "Gemiddelde dagen neerslag per maand Amsterdam",
+                label: "Gemiddelde hoeveelheid neerslag (mm) per maand Nederland",
                 backgroundColor: 'rgb(5,82,159,0.3)',
                 borderColor: 'rgb(5,82,159)',
                 borderWidth: '1',
                 pointRadius: '0',
                 fill: true,
                 yAxisID: 'Y-axis-1',
-                data: [19, 12, 15, 13, 14, 13, 14, 14, 16, 17, 18, 19]
+                data: [120, 90, 105, 115, 105, 135, 110, 125, 140, 105, 115, 118]
             }, {
                 type: "line",
-                label: 'Gemiddelde neerslag Londen',
+                label: 'Gemiddelde temperatuur Bangladesh',
                 backgroundColor: 'rgb(217,82,159)',
                 borderColor: 'rgb(217,79,151)',
                 borderWidth: '5.5',
@@ -37,7 +37,7 @@ var chart = new Chart(ctx, {
                 fill: false,
                 yAxisID: 'Y-axis-2',
                 order: 1,
-                data: [55.2, 40.9, 41.6, 43.7, 49.4, 45.1, 44.5, 49.5, 49.1, 68.5, 59, 55.2]
+                data: [20, 23, 26, 28, 28, 27, 26, 26, 26, 26, 24, 22]
             }, {
                 type: "line",
                 label: 'Gemiddelde neerslag Amsterdam',
@@ -48,7 +48,7 @@ var chart = new Chart(ctx, {
                 fill: false,
                 yAxisID: 'Y-axis-2',
                 order: 1,
-                data: [66.6, 50.9, 60.6, 40.9, 55.6, 66.0, 76.5, 85.9, 82.4, 89.6, 87.2, 76.3]
+                data: [12, 14, 16, 21, 24, 27, 28, 29, 26, 22, 17, 13]
             }],
         },
 
